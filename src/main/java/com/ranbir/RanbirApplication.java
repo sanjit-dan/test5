d2dc7ffa9414303d4339b58781f719f59a9e2059
Ranbir Sahu <ranbirsahu3@gmail.com>
2025-03-28 19:27:24 +0530

    first commit

---
+++ src/main/java/com/ranbir/RanbirApplication.java
@@ -0,0 +1,13 @@
+package com.ranbir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanbirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanbirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranbir/RanbirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranbir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanbirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
